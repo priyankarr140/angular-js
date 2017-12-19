5818e0b4e3370d014ce88059d05e4ce212ce98d4
PRIYANKA ARORA <priyankarr140@gmail.com>
2017-12-19 11:59:27 +0530

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{FormsModule} from '@angular/forms;
+import{FormsModule} from '@angular/forms';
 import{HttpModule} from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material'; 
@@ -9,11 +9,13 @@ import 'hammerjs';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
